d2ef588fcc554927ede19e5a3dffd78d6288946e
LTI-Intern-2021 <lntinterns2021@gmail.com>
2021-05-13 20:25:36 +0530

    Added getAllCustomers method

--- Customer_Application/src/main/java/com/example/Controller/customerController.java
+++ Customer_Application/src/main/java/com/example/Controller/customerController.java
@@ -1,7 +1,9 @@
 package com.example.Controller;
 
+import java.util.Collection;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.Entity.Customer;
 import com.example.service.CustomerService;
 
+@Slf4j
 @RestController
 @RequestMapping("/customer")
 public class customerController {
 	
 	@Autowired
 	public CustomerService customerService;
-	
-	
+
+	@GetMapping("/getAll")
+	public Collection<Customer>  getAllCustomers(){
+		log.info("Inside getAllCustomers method of CustomerController");
+		return customerService.getAllCustomers();
+	}
+
+
 	@PostMapping("/add")
 	public Customer addCustomer(@RequestBody Customer customer)
 	{
