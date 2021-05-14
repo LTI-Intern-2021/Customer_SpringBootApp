6484e03b970a5e09c9b762c207e8652725501dd5
LTI-Intern-2021 <83216793+LTI-Intern-2021@users.noreply.github.com>
2021-05-14 14:00:33 +0530

    Added CustomerUpdateInfo.java in Entity

---
+++ Customer_Application/src/main/java/com/example/Entity/CustomerUpdateInfo.java
@@ -0,0 +1,30 @@
+package com.example.Entity;
+
+public class CustomerUpdateInfo
+{
+    private int customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerLocation;
+    
+    public int getCustomerId() 
+    {
+		  return customerId;
+	  }
+    
+    public String getCustomerName() 
+    {
+		  return customerName;
+	  }
+    
+    public String getCustomerEmail() 
+    {
+		  return customerEmail;
+	  }
+    
+    public String getCustomerLocation()  
+    {
+		  return customerLocation;
+	  }
+  
+}
